5c7f655fdeedf5359a5e8fc0e417ab8c7cb927a8
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-11-05 14:26:56 +0000

    Developing main screen

--- GamesDevProjectSem1/GamesDevProjectSem1/Assets/Scripts/Character_Movement.cs
+++ GamesDevProjectSem1/GamesDevProjectSem1/Assets/Scripts/Character_Movement.cs
@@ -8,29 +8,59 @@ public class Character_Movement : MonoBehaviour
 {
     private Rigidbody2D m_RB;
 
-    private float m_InitialSpeed = 0.4f;
+    private float m_Speed = 1f;
 
-    private float m_Speed = 0.3f;
+    private float m_MaxSpeed = 10f;
 
-   
+    private bool m_EnableCapVelocity = true;
 
-    private void Start()
+    private void Awake()
     {
-        //Gets the attached rigidbody component
         m_RB = GetComponent<Rigidbody2D>();
-
-        Vector2 InitialImpulse = new Vector2(-(transform.position.x * m_InitialSpeed), 0.0f);
-
-        m_RB.AddForce(InitialImpulse, ForceMode2D.Impulse);
-
     }
-
 
     private void Update()
     {
-        
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else
+        {
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
     }
 
+    private void FixedUpdate()
+    {
+
+        ApplyMovement();
+
+        if (m_EnableCapVelocity)
+        {
+            CapVelocity();
+        }
+
+    }
+
+    public void ApplyMovement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+
+        float xForce = xInput * m_Speed;
+
+        Vector2 force = new Vector2(xForce, 0f);
+
+        m_RB.AddForce(force, ForceMode2D.Impulse);
+    }
+
+    public void CapVelocity()
+    {
+        float CappedXVelocity = Mathf.Min(Mathf.Abs(m_RB.velocity.x), m_MaxSpeed) * Mathf.Sign(m_RB.velocity.x);
+        float CappedYVelocity = m_RB.velocity.y;
+
+        m_RB.velocity = new Vector2(CappedXVelocity, CappedYVelocity);
+    }
 
 
 }
