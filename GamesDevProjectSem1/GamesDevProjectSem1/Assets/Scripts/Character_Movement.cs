2a9a78351f330d4808e6c2a047d4eead1d8153e0
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-11-06 02:47:04 +0000

    Player movement

--- GamesDevProjectSem1/GamesDevProjectSem1/Assets/Scripts/Character_Movement.cs
+++ GamesDevProjectSem1/GamesDevProjectSem1/Assets/Scripts/Character_Movement.cs
@@ -10,13 +10,22 @@ public class Character_Movement : MonoBehaviour
 
     private float m_Speed = 1f;
 
+    private float m_JumpForce = 5f;
+
     private float m_MaxSpeed = 10f;
 
     private bool m_EnableCapVelocity = true;
 
+    private BoxCollider2D boxCollider2D;
+
+    [SerializeField] private LayerMask groundLayerMask;
+
+    
+
     private void Awake()
     {
         m_RB = GetComponent<Rigidbody2D>();
+        boxCollider2D = transform.GetComponent<BoxCollider2D>();
     }
 
     private void Update()
@@ -29,6 +38,8 @@ public class Character_Movement : MonoBehaviour
         {
             gameObject.GetComponent<SpriteRenderer>().flipX = false;
         }
+
+        
     }
 
     private void FixedUpdate()
@@ -41,10 +52,13 @@ public class Character_Movement : MonoBehaviour
             CapVelocity();
         }
 
+        
+
     }
 
     public void ApplyMovement()
     {
+        //Move horizontally
         float xInput = Input.GetAxis("Horizontal");
 
         float xForce = xInput * m_Speed;
@@ -52,15 +66,37 @@ public class Character_Movement : MonoBehaviour
         Vector2 force = new Vector2(xForce, 0f);
 
         m_RB.AddForce(force, ForceMode2D.Impulse);
+
+        //Jump
+
+        if(IsGrounded())
+        {
+            Vector2 jumpDirection = new Vector2(0f, Input.GetAxis("Jump"));
+
+            m_RB.AddForce(jumpDirection * m_JumpForce, ForceMode2D.Impulse);
+        }
+        
+        
+            
+
     }
 
     public void CapVelocity()
     {
+        //Take the minimun between those 2 values, multiply by velocity sign in x
         float CappedXVelocity = Mathf.Min(Mathf.Abs(m_RB.velocity.x), m_MaxSpeed) * Mathf.Sign(m_RB.velocity.x);
         float CappedYVelocity = m_RB.velocity.y;
 
         m_RB.velocity = new Vector2(CappedXVelocity, CappedYVelocity);
     }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0f, Vector2.down, 0.1f, groundLayerMask);
+        return raycastHit2d.collider != null;
+
+    }
+   
 
 
 }
