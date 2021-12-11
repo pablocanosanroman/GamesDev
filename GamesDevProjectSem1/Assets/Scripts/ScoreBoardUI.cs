b47b06cffd8c1ade4e1bc855426c4034e3455def
pablocanosanroman <c015653k@student.staffs.ac.uk>
2021-12-11 00:37:09 +0000

    Scoreboards

--- GamesDevProjectSem1/Assets/Scripts/ScoreBoardUI.cs
+++ GamesDevProjectSem1/Assets/Scripts/ScoreBoardUI.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class ScoreBoardUI : MonoBehaviour
 {
     [SerializeField] private SceneSwitcher m_SceneSwitcher;
+    [SerializeField] private Text m_NameText;
+    [SerializeField] private Text m_TimeText;
+    [SerializeField] private Transform m_HighScoreHolder;
+    [SerializeField] private GameObject m_ScoreboardEntry;
     public void GetToMainMenu()
     {
         SceneManager.LoadScene(0);
@@ -16,5 +21,11 @@ public class ScoreBoardUI : MonoBehaviour
     {
         SceneManager.LoadScene("Level" + m_SceneSwitcher.m_CurrentLevel.ToString(), LoadSceneMode.Single);
         Time.timeScale = 1;
+    }
+
+    public void Load()
+    {
+        
+
     }
 }
